1f456c86f516da9382bddbe86fa7c24773ed9ad4
mikelituarte <mikelituarte@gmail.com>
2015-06-19 20:44:43 +0200

    19-06-2015 20:44

--- ProyectoServlets/src/miservlet/MiServlet.java
+++ ProyectoServlets/src/miservlet/MiServlet.java
@@ -1,5 +1,6 @@
 package miservlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 public class MiServlet extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		System.out.println("Ha llamado a doGet() de la clase MiServlet dentro del paquete miservlet");
-		super.doGet(req, resp);
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
+		String nombre = "";
+		String respuesta = null;
+		nombre = req.getParameter("nombre");
+		respuesta = "El numero de letras que tiene el nombre \""+nombre+"\" es: "+ nombre.length();
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.print(respuesta);
+		//super.doGet(req, resp);
 	}
 	
 	@Override
